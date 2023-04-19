d52075b11fe69682ef4ce96cf4f55292823cdca8
dharan-dhakshan97 <dharanidhakshan97@gmail.com>
2023-04-19 14:54:21 +0530

    add initial commit

---
+++ hookss/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Effectsapicall from "./Hooks/Effects/Effectsapicall";
+
+const App = () => {
+  return (
+    <>
+    <Effectsapicall/>
+    </>
+  )
+}
+
+export default App;
